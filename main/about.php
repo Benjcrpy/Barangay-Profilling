a0b3998e78a0416fc6a33105d85169eb5d953fb6
BENJ <AnthonyAgapay70929@gmail.com>
2023-01-04 07:04:12 +0800

    first commit

---
+++ main/about.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+  
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- font Awesome -->
+    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Ionicons -->
+    <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../js/morris/morris-0.4.3.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="../css/AdminLTE.css" rel="stylesheet" type="text/css" />
+
+    <link href="../css/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="../css/select2.css" rel="stylesheet" type="text/css" />
+    <script src="../js/jquery-1.12.3.js" type="text/javascript"></script>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link href="../css/style.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+<nav class="navbar navbar-inverse" style="border-radius:0px;">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php"><img alt="Brand" src="../img/logo.png" style="width:50px; margin-top:-15px; "></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="../login.php">Admin</a></li>
+        <li><a href="../pages/resident/login.php">Resident</a></li>
+        <li><a href="../pages/zone/login.php">Zone Leader</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+<div class="heading" style="background:url(../images/header-bg-3.png) no-repeat">
+   <h1>about us</h1>
+</div>
+
+<div class="new1">
+   </div>
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="image">
+      <img src="../images/Brgy.jfif" alt="">
+   </div>
+
+   <div class="content">
+      <h3>Vission</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure numquam nulla iusto corporis dolor commodi libero, vitae obcaecati optio rerum ab culpa nesciunt, earum mollitia quasi ipsam non. Aliquid, iure.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid rerum, delectus voluptate aliquam quaerat iusto repellendus error nulla ab atque.</p>
+
+      <h3>Mission</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure numquam nulla iusto corporis dolor commodi libero, vitae obcaecati optio rerum ab culpa nesciunt, earum mollitia quasi ipsam non. Aliquid, iure.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid rerum, delectus voluptate aliquam quaerat iusto repellendus error nulla ab atque.</p>
+
+      <h3>History</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure numquam nulla iusto corporis dolor commodi libero, vitae obcaecati optio rerum ab culpa nesciunt, earum mollitia quasi ipsam non. Aliquid, iure.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid rerum, delectus voluptate aliquam quaerat iusto repellendus error nulla ab atque.</p>
+</div>
+
+
+</section>
+
+<!-- about section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="../login.php"> <i class="fas fa-angle-right"></i> Admin</a>
+         <a href="../pages/resident/login.php"> <i class="fas fa-angle-right"></i> Resident</a>
+         <a href="../pages/zone/login.php"> <i class="fas fa-angle-right"></i> Zone Leader</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +63912-345-67891</a>
+         <a href="#"> <i class="fas fa-envelope"></i> juandelacruz12@gmail.com </a>
+         <a href="https://www.google.com/maps/place/564+Barangay+Hall+Manila/@14.6088961,121.0044211,18z/data=!4m13!1m7!3m6!1s0x3397b61e702feaf1:0x51c95967c2b1e5f4!2sBrgy.+564,+Sampaloc,+Manila,+1008+Metro+Manila!3b1!8m2!3d14.6088808!4d121.005376!3m4!1s0x3397b61e73fd20f9:0xc7057a0463938bf5!8m2!3d14.6083739!4d121.0053345"> <i class="fas fa-map"></i> Manila, Philippines </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+   </div>
+
+</section>
+
+<!-- footer section ends -->
+
+<!-- jswiper s link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="../js/script.js"></script>
+
+</body>
+</html>
---
+++ main/index.php
@@ -0,0 +1,162 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Barangay Information System</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/homepage.css" rel="stylesheet" type="text/css" />
+    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../js/morris/morris-0.4.3.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/AdminLTE.css" rel="stylesheet" type="text/css" />
+
+    <link href="../css/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="../css/select2.css" rel="stylesheet" type="text/css" />
+    <script src="../js/jquery-1.12.3.js" type="text/javascript"></script>
+
+    
+    
+</head>
+<body>
+<nav class="navbar navbar-inverse" style="border-radius:0px;">
+  <div class="container-fluid">
+
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php"><img alt="Brand" src="../img/logo1.png" style="width:50px; margin-top:-15px; "></a>
+    </div>
+
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="text-align: right">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home <span class="sr-only"></span></a></li>
+        <li ><a href="about.php">About</a></li> 
+        <li class= "b"><a href="../login.php">Admin</a></li>
+        <li class= "b"><a href="../pages/resident/login.php">Resident</a></li>
+        <li class= "b"><a href="../pages/zone/login.php">Zone Leader</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</div>
+
+<header class="main" >
+
+      <div class="main-heading">
+            <h1>Request Barangay Needs with an ease of a button</h1>
+            <h3> Scroll Down Below ⬇️ </h3>
+            
+        </div>
+        <div style="width:300px; text-align:right;">
+        <img src="../img/click.png">
+</div>
+        
+    </header>
+    
+</body>
+
+
+<section class="features">
+        <div class="feature-container">
+
+            <div class="feature-box">
+                <div class="f-img">
+                    <img src="../img/Permit.png"/>
+                </div>
+                <div class="f-text">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <a href="../pages/resident/login.php" class="main-btn">Permit</a>
+                </div>
+            </div>
+
+            <div class="feature-box">
+                <div class="f-img">
+                    <img src="../img/Blotter.png"/>
+                </div>
+                <div class="f-text">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <a href="../pages/resident/login.php" class="main-btn">Blotter</a>
+                </div>
+            </div>
+
+            <div class="feature-box">
+                <div class="f-img">
+                    <img src="../img/Clearance.png"/>
+                </div>
+                <div class="f-text">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <a href="../pages/resident/login.php" class="main-btn">Clearance</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class ="index-intro">
+<div class="index-news">
+      <div class = "wrapper">
+ 
+<article>
+    <div class="news-link"> </div>
+    <h3> Marcos to PhilHealth: Suspend premium rate hike </h3>
+    <p> President Ferdinand Marcos Jr. has ordered the Philippine Health Insurance Corp. (PhilHealth) to suspend the increase in premium rate to be implemented this year.</p>
+    <a href="https://www.manilatimes.net/2023/01/02/news/marcos-to-philhealth-suspend-premium-rate-hike/1872555" class="news-button">Read Now</a>
+</article>
+
+<article>
+    <div class="news-link"> </div>
+    <h3> Let us move full-steam ahead </h3>
+    <p> PRESIDENT Ferdinand "Bongbong" Marcos Jr. called on Filipinos to move "full-steam ahead" toward growth and prosperity in 2023.
+
+In his first New Year's message as president, Marcos said he joined the entire nation to welcome the new year with hope and optimism.</p>
+    <a href="https://www.manilatimes.net/2023/01/01/news/let-us-move-full-steam-ahead/1872392" class="news-button">Read Now</a>
+</article>
+
+<article>
+    <div class="news-link"> </div>
+    <h3> Airlines mount recovery flights </h3>
+    <p> IT will take at least three days before airlines finish the recovery flights for stranded passengers affected by the power outage that canceled flights to and from Manila on New Year's Day, Transportation Secretary Jaime Bautista said on Monday.</p>
+    <a href="https://www.manilatimes.net/2023/01/03/news/airlines-mount-recovery-flights/1872567" class="news-button">Read Now</a> 
+</article>
+
+<article>
+    <div class="news-link"> </div>
+    <h3> Onion prices remain high despite P250 SRP </h3>
+    <p>  THE retail prices of onions remained high despite the implementation of the P250 suggested retail price (SRP) on Dec. 30, 2022. An official of the Department of Agriculture (DA) warned that traders could face profiteering and manipulation charges.</p>
+    <a href="https://www.manilatimes.net/2023/01/03/news/onion-prices-remain-high-despite-p250-srp/1872495" class="news-button">Read Now</a>
+</article>
+
+<article>
+    <div class="news-link"> </div>
+    <h3> Technical problems delay flights </h3>
+    <p> Technical problems that hit the Air Traffic Management Center delayed flights on the first day of 2023.
+
+The Civil Aviation Authority of the Philippines apologized to affected passengers of domestic and international flights for the inconvenience.</p>
+<a href="https://www.manilatimes.net/2023/01/01/news/technical-problems-delay-flights/1872421" class="news-button">Read Now</a>
+</article>
+</div>
+</section>
+
+<script src="../js/alert.js" type="text/javascript"></script>
+
+
+
+<script src="../js/morris/raphael-2.1.0.min.js" type="text/javascript"></script>
+<script src="../js/morris/morris.js" type="text/javascript"></script>
+<script src="../js/select2.full.js" type="text/javascript"></script>
+
+<script src="../js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="../js/dataTables.buttons.min.js" type="text/javascript"></script>
+<script src="../js/buttons.print.min.js" type="text/javascript"></script>
+<script src="../js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
+<script src="../js/AdminLTE/app.js" type="text/javascript"></script>
+
+
+
+</script>
+</html>
